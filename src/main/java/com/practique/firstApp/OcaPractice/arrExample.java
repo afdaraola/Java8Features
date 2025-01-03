e3c2bda048d99d88bde1558d96ca69554feb0dd6
Festus <daramolafestus@yahoo.com>
2025-01-02 19:12:59 -0500

    added a new class

---
+++ src/main/java/com/practique/firstApp/OcaPractice/arrExample.java
@@ -0,0 +1,13 @@
+package com.practique.firstApp.OcaPractice;
+
+public class arrExample {
+    public static void main(String[] args) {
+
+        int arr[] = {2,1,0};
+
+        for ( int i : arr){
+           // System.out.println(arr[i]);  // 0,1,2
+            System.out.println(i);  // 2,1,0
+        }
+    }
+}
